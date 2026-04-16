da4852c5b063db368da1526be801ffa97d871ea2
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-04-16 22:43:10 +0700

    feat: add displayname field to User entity

--- backend/src/main/java/com/mezon/backend/entity/User.java
+++ backend/src/main/java/com/mezon/backend/entity/User.java
@@ -3,6 +3,7 @@ package com.mezon.backend.entity;
 public record User(
         Long id,
         String username,
+        String displayname,
         String avatar,
         String password,
         String email,

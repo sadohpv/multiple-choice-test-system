83228300f15f42e352523077bd08b016f65ded55
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-08 01:25:16 +0700

    feat: add ForgotPasswordPage route to publicRoutes for enhanced user navigation

--- frontend/src/routes/publicRoutes.ts
+++ frontend/src/routes/publicRoutes.ts
@@ -1,6 +1,7 @@
 import { AUTH_PATHS } from "@/constants/path";
 import { LoginPage } from "@/features/auth/pages/LoginPage";
 import { RegisterPage } from "@/features/auth/pages/RegisterPage";
+import { ForgotPasswordPage } from "@/features/auth/pages/ForgotPasswordPage";
 
 export const publicRoutes = [
     {
@@ -10,5 +11,9 @@ export const publicRoutes = [
     {
         path: AUTH_PATHS.register.slice(1),
         component: RegisterPage,
+    },
+    {
+        path: AUTH_PATHS.forgotPassword.slice(1),
+        component: ForgotPasswordPage,
     },
 ];

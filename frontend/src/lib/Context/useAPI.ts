845badeac6c98efcb07c1ffd548a803fa56312f2
Nguyễn Hùng <nguyentanhung413@gmail.com>
2026-05-14 13:52:33 +0700

    refactor: remove useApi function and related context in useAPI.ts

--- frontend/src/lib/Context/useAPI.ts
+++ frontend/src/lib/Context/useAPI.ts
@@ -1,12 +1,5 @@
 import { useContext } from "react";
-import ApiContext from "./ContextApi";
 import AuthContext from "./ContextAuth";
-
-export const useApi = () => {
-    const ctx = useContext(ApiContext);
-    if (!ctx) throw new Error("useApi must be used within ApiProvider");
-    return ctx;
-};
 
 export const useAuth = () => {
     const ctx = useContext(AuthContext);
